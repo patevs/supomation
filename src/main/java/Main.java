e384c977abc8c7e1767a3569e4bd9592db1a57d9
Patrick <ppevans11@gmail.com>
2019-03-02 15:30:43 +1300

    added Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+/**
+ *  src/main/java/Main.java
+ *  application entry point
+ */
+
+public class Main {
+
+    //private static final String URL = "http://localhost:8181";
+
+    public static void main(String[] args){
+        System.out.println("\nStarting Supo Scrapper...");
+    }
+
+}
