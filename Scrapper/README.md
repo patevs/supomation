694cbb19af3bd64f48cb0048eb675521f30a624e
Patrick <ppevans11@gmail.com>
2019-03-05 19:43:58 +1300

    updates

---
+++ Scrapper/README.md
@@ -0,0 +1,20 @@
+
+# Scrapper
+
+Web scrapper written in python.
+
+----
+
+### Links & Resources
+
+* Selenium
+  * https://pypi.org/project/selenium/
+  * https://github.com/SeleniumHQ/selenium/
+
+* WebDriverManager
+  * https://pypi.org/project/webdrivermanager/
+  * https://github.com/Omenia/webdrivermanager
+
+
+----
+
--- Scrapper/main.py
+++ Scrapper/main.py
@@ -3,15 +3,46 @@
     Scrapper/main.py
 '''
 
+
 # -------------------------------
 
 # IMPORTS
 
+from webdrivermanager import GeckoDriverManager
+from selenium import webdriver
+
 # -------------------------------
+
+
+# GLOBAL VARIABLES
+
+gdd = GeckoDriverManager()
+driver = None
+
+target = "https://www.newworld.co.nz/"
+mailer = "savings/virtualmailer/?store=thorndon-1260"
+
 
 # Main method
 def main():
-    print("\nStarting Scrapper...")
+    print("\n  ---- Supo Scrapper ----")
+
+    print("\n\tDownload and install GeckoDriver...")
+    gdd.download_and_install()
+
+    print("\n\tStarting Firefox...")
+    driver = webdriver.Firefox()
+
+    print("\n\tNavigate to virtual mailer...")
+    driver.get(target + mailer)
+
+    print("\n\tClosing Firefox...")
+    driver.quit()
+
+    print("\n  -------- Done. --------\n")
+
+    return 0
+
 
 # Application entry point
 if __name__ == "__main__":
