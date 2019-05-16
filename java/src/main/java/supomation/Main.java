96d41191988293a0ef6dc135e4125f0c7c20df09
Patrick <ppevans11@gmail.com>
2019-05-16 18:10:08 +1200

    ✨  Updates to pom.xml

--- java/src/main/java/supomation/Main.java
+++ java/src/main/java/supomation/Main.java
@@ -1,5 +1,9 @@
 package supomation;
 
+import asg.cliche.Command;
+import asg.cliche.ShellFactory;
+import java.io.IOException;
+
 /**
  * * Main.java
  *
@@ -14,11 +18,23 @@ public class Main {
      * * METHODS *
      *************/
 
+    @Command // One,
+    public String hello() {
+        return "Hello, World!";
+    }
+
+    @Command // two,
+    public int add(int a, int b) {
+        return a + b;
+    }
+
     /**
      * main method - application entry point
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("\n\t-> Starting Supomation...");
+        // ..
+        ShellFactory.createConsoleShell("hello", "", new Main()).commandLoop(); // and three.
         // ..
         System.out.println("\n\t-> Stopping Supomation...");
         // ..
