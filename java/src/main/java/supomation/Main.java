00f754ad7437b55332ab78f424b8bd06567ff7a6
Patrick <ppevans11@gmail.com>
2019-05-16 17:48:29 +1200

    ✨  Create new maven project

---
+++ java/src/main/java/supomation/Main.java
@@ -0,0 +1,27 @@
+/**
+ * * Main.java
+ *
+ * Main class contains the application main method which is the application
+ * entry point.
+ *
+ * @author patevs
+ */
+public class Main {
+
+    /*************
+     * * METHODS *
+     *************/
+
+    /**
+     * main method - application entry point
+     */
+    public static void main(String[] args) {
+        System.out.println("\n\t-> Starting Supomation...");
+        // ..
+        System.out.println("\n\t-> Stopping Supomation...");
+        // ..
+    }
+    // ..
+}
+
+/* EOF */
