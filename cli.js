e20325df3105a0d2cb910244339c9a7a1739de5b
PatEvs <ppevans11@gmail.com>
2019-06-26 17:12:04 +1200

    ✨  Updates to cli.js

--- cli.js
+++ cli.js
@@ -9,16 +9,19 @@
 /*************
  * * IMPORTS *
  *************/
+
 // const puppeteer = require("puppeteer");
 // const logSymbols = require("log-symbols");
-// const chalk = require("chalk");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 //const fs = require("fs");
 
 /***********
  * * THEME *
  ***********/
-// const log = console.log;
-// const title = chalk.bold.underline.green;
+
+const log = console.log;
+const title = chalk.bold.underline.green;
 // const link = chalk.underline.blue;
 // const green = chalk.green;
 //const red = chalk.red;
@@ -26,6 +29,7 @@
 /***************
  * * CONSTANTS *
  ***************/
+
 // Target URL
 // const TARGET = "https://www.ishopnewworld.co.nz/specials";
 // Product CSS selector
@@ -35,14 +39,45 @@
  * * FUNCTIONS *
  ***************/
 
+function cli() {
+	inquirer
+		.prompt([
+			// Pass your questions in here
+			{
+				type: "confirm",
+				name: "toBeDelivered",
+				message: "Is this for delivery?",
+				default: false
+			},
+		])
+		.then(answers => {
+			// Use user feedback for... whatever!!
+			log("\nOrder receipt:");
+			log(JSON.stringify(answers, null, "  "));
+		});
+}
+
 /**
 * * Application entry point
 */
-/*
-(async () => {
-  log(title("\nSUPOMATION\n"));
-  // runSupomation();
+(function () {
+	log(title("\n  SUPOMATION\n"));
+	cli();
+	// runSupomation();
 })();
+
+/* self invoking async function
+(async () => {
+  //..
+})();
+*/
+
+/* ora usage
+const spinner = ora("Loading CLI...").start();
+setTimeout(() => {
+	spinner.succeed();
+	//..
+}, 2000);
 */
 
 // EOF //
