ea99e4ef342bdd277a6ddc03461e477f735ea4e9
PatEvs <ppevans11@gmail.com>
2019-06-26 16:33:55 +1200

    ✨  Created cli.js

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+/**
+ *  `cli.js`
+ */
+
+"use strict";
+
+/*************
+ * * IMPORTS *
+ *************/
+// const puppeteer = require("puppeteer");
+// const logSymbols = require("log-symbols");
+// const chalk = require("chalk");
+//const fs = require("fs");
+
+/***********
+ * * THEME *
+ ***********/
+const log = console.log;
+// const title = chalk.bold.underline.green;
+// const link = chalk.underline.blue;
+// const green = chalk.green;
+//const red = chalk.red;
+
+/***************
+ * * CONSTANTS *
+ ***************/
+// Target URL
+const TARGET = "https://www.ishopnewworld.co.nz/specials";
+// Product CSS selector
+const productBaseSelector = ".fs-product-card";
