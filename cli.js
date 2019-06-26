f76edc7ac2cba4bbf7f51afef8bc4c09d9b5be7a
PatEvs <ppevans11@gmail.com>
2019-06-26 22:42:38 +1200

    ✨  Updates to cli.js

--- cli.js
+++ cli.js
@@ -23,8 +23,8 @@ const chalk = require("chalk");
 
 const log = console.log;
 const title = chalk.bold.underline.green;
+const link = chalk.underline.blue;
 const green = chalk.green;
-// const link = chalk.underline.blue;
 //const red = chalk.red;
 
 /***************
@@ -32,7 +32,7 @@ const green = chalk.green;
  ***************/
 
 // Target URL
-// const TARGET = "https://www.ishopnewworld.co.nz/specials";
+const TARGET = "https://www.ishopnewworld.co.nz/specials";
 // Product CSS selector
 // const productBaseSelector = ".fs-product-card";
 
@@ -55,8 +55,77 @@ async function initPuppeteer() {
 }
 
 /**
+ * Navigate puppeteer to a given page
+ * @param { puppeteer page instance } page
+ * @param { url to naviagte to } url
+ */
+async function gotoPage(page, url) {
+	log(logSymbols.info, "Navigating browser page to: \n\t" + link(url));
+	await page.goto(url);
+}
+
+// eslint-disable-next-line no-unused-vars
+async function runSupomation() {
+	// launch puppeteer browser instance
+	const browser = await initPuppeteer();
+	// log(await browser.version()); // check we have the browser instance
+	// create a new page
+	const page = await browser.newPage();
+	// Navigate to url target
+	await gotoPage(page, TARGET);
+}
+
+/**
+ * Process main menu options
+ * @param { selected user option } option
+ */
+function processMenuOption(option) {
+	if (option === "q") {
+		log(logSymbols.error, "Quitting Supomation CLI...\n");
+		process.exit(0);
+	} else if (option === "s") {
+		log(logSymbols.info, "Starting Supomation WebScrapper...");
+		runSupomation();
+	}
+}
+
+/**
+ * Displays the supomation cli main menu
+ */
+function mainMenuPrompt() {
+	inquirer
+		.prompt([
+			{
+				type: "list",
+				name: "option",
+				message: "Select an option:",
+				choices: [
+					"Start WebScrapper",
+					"Quit Supomation CLI",
+					new inquirer.Separator(),
+					{
+						name: "Display Help",
+						disabled: "Unavailable at this time"
+					}
+				],
+				filter: function (val) {
+					return val.charAt(0).toLowerCase();
+				}
+			}
+		])
+		.then(answers => {
+			// console.log(JSON.stringify(answers, null, "  "));
+			processMenuOption(answers.option);
+		});
+}
+
+/**
  * Start the supomation interactive prompt
  */
+function startSupomationCLI() {
+	mainMenuPrompt();
+}
+/*
 function startSupomationCLI() {
 	inquirer
 		.prompt([
@@ -89,6 +158,7 @@ function startSupomationCLI() {
 			}
 		});
 }
+*/
 
 /**
 * * Application entry point
@@ -96,7 +166,6 @@ function startSupomationCLI() {
 (function () {
 	log(title("\nWELCOME TO SUPOMATION CLI\n"));
 	startSupomationCLI();
-	// initPuppeteer();
 })();
 
 /* self invoking async function
