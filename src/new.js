401ce02e72e2c0f58d61ea92d6f1764c86e82d3d
PatEvs <ppevans11@gmail.com>
2019-06-27 18:32:55 +1200

    ✨  Created src/new.js

---
+++ src/new.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+/**
+ *  `new.js`
+ */
+
+"use strict";
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// const puppeteer = require("puppeteer");
+// const inquirer = require("inquirer");
+// const ora = require("ora");
+// const logSymbols = require("log-symbols");
+const chalk = require("chalk");
+// const fs = require("fs");
+
+/***********
+ * * THEME *
+ ***********/
+
+const log = console.log;
+const title = chalk.bold.underline.green;
+// const link = chalk.underline.cyan;
+// const green = chalk.green;
+// const red = chalk.red;
+// const blue = chalk.blue;
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+// Target URL
+// const TARGET = "https://www.ishopnewworld.co.nz/specials";
+// Product CSS selector
+// const productBaseSelector = ".fs-product-card";
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/**
+* * Application entry point
+*/
+(function () {
+	log(title("\nWELCOME TO SUPOMATION CLI\n"));
+	// startSupomationCLI();
+})();
+
+
+// EOF //
+
