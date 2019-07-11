74273ccd08246cd672cd728b16d529fb68b03e58
PatEvs <ppevans11@gmail.com>
2019-07-12 03:39:39 +1200

    ✨  Created 0.3/

---
+++ 0.3/lib/__tests__/supomation.test.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+const assert = require("assert");
+const supomation = require("../index.js");
+
+describe("supomation", () => {
+	it("has a test", () => {
+		assert(true, "supomation should have a test");
+	});
+});
