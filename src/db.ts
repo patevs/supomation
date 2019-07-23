17ea839d9a098c5df3ac69e733272dc0f1db1407
PatEvs <ppevans11@gmail.com>
2019-07-23 18:08:34 +1200

    ✨  Updates

--- src/db.ts
+++ src/db.ts
@@ -13,29 +13,25 @@
  *************/
 
 // const MongoClient = require("mongodb").MongoClient;
-// import MongoClient from "mongodb";
+import { MongoClient } from "mongodb";
 
 /***************
  * * CONSTANTS *
  ***************/
 
-/*
 const uri =
-    "mongodb+srv://patevs:<password>@cluster0-t7g5a.mongodb.net/test?retryWrites=true&w=majority";
+    "mongodb+srv://patevs:F0rtunes@cluster0-t7g5a.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
-*/
 
 /***************
  * * FUNCTIONS *
  ***************/
 
-/*
 // Connect to database
 client.connect(err => {
     const collection = client.db("test").collection("devices");
     // perform actions on the collection object
     client.close();
 });
-*/
 
 // EOF //
