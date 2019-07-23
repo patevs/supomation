d5623fe3b5dff6ab724d6d4b436ed9662c8f5f48
PatEvs <ppevans11@gmail.com>
2019-07-23 15:35:19 +1200

    ✔️  Added cheerio dep

--- src/index.ts
+++ src/index.ts
@@ -86,6 +86,17 @@ function getVirtualMailer() {
     createAxiosInstance();
 }
 
+/* axios async example
+async function getUser() {
+    try {
+        const response = await axios.get("/user?ID=12345");
+        console.log(response);
+    } catch (error) {
+        console.error(error);
+    }
+}
+*/
+
 /**********************
  * * PROMPT FUNCTIONS *
  **********************/
