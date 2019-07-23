ba7262ebe17c4413d4a84506267107d892372479
PatEvs <ppevans11@gmail.com>
2019-07-23 18:03:03 +1200

    ✨  Created src/db.ts

---
+++ src/db.ts
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+/**
+ *  `src/db.ts`
+ *
+ *  * Supomation Database Connection
+ */
+
+"use strict";
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// const MongoClient = require("mongodb").MongoClient;
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+/*
+const uri =
+    "mongodb+srv://patevs:<password>@cluster0-t7g5a.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+*/
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/*
+// Connect to database
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+*/
+
+// EOF //
