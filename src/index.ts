d431bd8f376b2442e4a54fa6ae25eab8e77c58d9
PatEvs <ppevans11@gmail.com>
2019-07-24 13:52:22 +1200

    ✨  Updates

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,40 @@
 // Logging Utilities
 import * as logging from './utils/logging';
 
+/***************
+ * * CONSTANTS *
+ ***************/
+
+const BASE_URL = 'https://www.ishopnewworld.co.nz';
+const CATEGORY_BASE_URL = BASE_URL + '/category/';
+
+// Array of all categories
+const ALL_CATEGORIES = [
+    'fresh-foods-and-bakery',
+    'chilled-frozen-and-desserts',
+    'pantry',
+    'personal-care',
+    'kitchen-dining-and-household'
+];
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+const runWebScraper = () => {
+    const category = ALL_CATEGORIES[0];
+    const target = CATEGORY_BASE_URL + category;
+    logging.log(target);
+};
+
 /*****************************
  * * APPLICATION ENTRY POINT *
  *****************************/
 
 (function() {
     logging.logWelcome(); // Log Supomation main welcome
+    logging.logInfo('Starting Supomation WebScraper...');
+    runWebScraper();
     // promptMain(); // Main menu prompt
 })();
 
