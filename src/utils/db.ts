e9034c140ef13922266677d8bf83bae22e8e8ef7
PatEvs <ppevans11@gmail.com>
2019-07-24 17:15:10 +1200

    ✨  Updates to db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -12,18 +12,40 @@
  * * IMPORTS *
  *************/
 
-//
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+// const assert = require('assert');
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+// Database Name
+const dbName = 'myproject';
 
 /***************
  * * FUNCTIONS *
  ***************/
 
-//
+const connectToDb = () => {
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function(err, client) {
+        assert.equal(null, err);
+        console.log('Connected successfully to server');
+
+        // const db =
+        client.db(dbName);
+
+        client.close();
+    });
+};
 
 /*************
  * * EXPORTS *
  *************/
 
-// export {  };
+export { connectToDb };
 
 // EOF //
