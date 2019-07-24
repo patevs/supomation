43ec7f7bb6b95ea521ce10a0f93bd40b46e65656
PatEvs <ppevans11@gmail.com>
2019-07-24 13:23:37 +1200

    ✨  Updates scripts

--- src/__tests__/index-spec.ts
+++ src/__tests__/index-spec.ts
@@ -1,5 +1,27 @@
+/*************
+ * * IMPORTS *
+ *************/
+
 import * as index from '../index';
+
+// const assert = require('assert');
+// . const supomation = require("../index.ts");
+// . import supomation from "../index.ts";
+
+/****************
+ * * TEST CASES *
+ ****************/
 
 test('Should have Greeter available', () => {
     expect(index.Greeter).toBeTruthy();
 });
+
+/*
+describe('supomation', () => {
+    it('has a test', () => {
+        assert(true, 'supomation should have a test');
+    });
+});
+*/
+
+// EOF //
