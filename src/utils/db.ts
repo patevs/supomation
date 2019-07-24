6aa2c46e0dd5124774dcd72f108e954231ffad42
PatEvs <ppevans11@gmail.com>
2019-07-24 17:11:41 +1200

    ✔️  Created src/utils/db.ts & added mongodb dep

---
+++ src/utils/db.ts
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+/**
+ *	`src/utils/db.ts`
+ *
+ *	* Supomation CLI Database Helper Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+//
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+//
+
+/*************
+ * * EXPORTS *
+ *************/
+
+// export {  };
+
+// EOF //
