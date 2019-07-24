878941f9729dfb0c51c5a6799e0d2fa9b6d44661
PatEvs <ppevans11@gmail.com>
2019-07-25 00:29:41 +1200

    ✨  Updates to src/utils/utilities.ts

--- src/utils/utilities.ts
+++ src/utils/utilities.ts
@@ -20,6 +20,52 @@ import writeJsonFile from 'write-json-file';
  ***************/
 
 /**
+ *	* quit
+ *  Quit Supomation CLI
+ *  @returns { void }
+ */
+/*
+function quit(): void {
+    logging.logError('Quitting Supomation CLI...');
+    process.exit(0);
+}
+*/
+
+// ------------------------------------------ //
+
+/**
+ *	*version
+ *  Print the Supomation CLI version
+ *  @returns { void }
+ */
+/*
+function version(): void {
+    logging.log(); // new line
+    const version = process.env.npm_package_version;
+    const msg = logging.magenta('Supomation CLI version: ') + version;
+    logging.logInfo(msg);
+    logging.log(); // new line
+}
+*/
+
+// ------------------------------------------ //
+
+/**
+ *	* help
+ *  Print the Supomation help message
+ *  @returns { void }
+ */
+/*
+function help(): void {
+    logging.log(); // new line
+    logging.logInfo(' ---- ' + logging.magenta('HELP MESSAGE') + ' ---- ');
+    logging.log(); // new line
+}
+*/
+
+// ------------------------------------------ //
+
+/**
  *  * saveProductData
  * @param { string } fileName
  * @param { any } productData
