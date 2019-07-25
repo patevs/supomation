9a9eb1b6c14cb6aa0cc92faced19c0e7f3c7c963
PatEvs <ppevans11@gmail.com>
2019-07-25 18:14:53 +1200

    ✨  🔥 Deleted examples/

--- examples/__tests__/greeter-spec.ts
+++
@@ -1,6 +0,0 @@
-import { Greeter } from '../greeter';
-
-test('Should greet with message', () => {
-    const greeter = new Greeter('friend');
-    expect(greeter.greet()).toBe('Bonjour, friend!');
-});
