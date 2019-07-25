998668b39f2fedf82ef5238760fd2171a5289024
PatEvs <ppevans11@gmail.com>
2019-07-25 22:35:15 +1200

    ✨  multispinners

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ import axios from 'axios';
 // Interactive logging
 import { Signale } from 'signale';
 
-import multispinner from 'multispinner';
+import Multispinner from 'multispinner';
 
 /***************
  * * CONSTANTS *
@@ -62,6 +62,26 @@ const getPageContents = async (targetUrl: string) => {
 };
 
 // ------------------------------------------ //
+
+const initSpinnie = () => {
+    const spinners = ['foo', 'bar', 'baz', 'qux'];
+    // instantiate and start spinners
+    const m = new Multispinner(spinners);
+
+    // finish spinners in staggered timeouts
+    setTimeout(() => m.success('foo'), 1000);
+    setTimeout(() => m.error('bar'), 1500);
+    setTimeout(() => m.success('baz'), 2000);
+    setTimeout(() => m.error('qux'), 2500);
+
+    // do something on success/error events
+    m.on('success', () => {
+        // does not fire in this example because m.error is called
+        console.log('done without errors!');
+    }).on('err', e => {
+        console.log(`${e} spinner finished with an error`);
+    });
+};
 
 /**
  *  *initPrompt
