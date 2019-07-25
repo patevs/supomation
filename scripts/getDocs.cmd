2049fe32a6893255cc18668c36555220ff93be4a
PatEvs <ppevans11@gmail.com>
2019-07-25 21:38:21 +1200

    ✨  Updates to getDocs.cmd

--- scripts/getDocs.cmd
+++ scripts/getDocs.cmd
@@ -7,31 +7,31 @@ ECHO. && ECHO Downloading Documentation for Project Dependencies...
 :: CALL curl https://raw.githubusercontent.com/user/repository/branch/filename
 
 ECHO. && ECHO Axios:
-CALL curl https://raw.githubusercontent.com/axios/axios/master/README.md -o axios.md -#
+CALL curl https://raw.githubusercontent.com/axios/axios/master/README.md -o docs\axios.md -#
 
 ECHO. && ECHO Boxen:
-CALL curl https://raw.githubusercontent.com/sindresorhus/boxen/master/readme.md -o boxen.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/boxen/master/readme.md -o docs\boxen.md -#
 
 ECHO. && ECHO Chalk:
-CALL curl https://raw.githubusercontent.com/chalk/chalk/master/readme.md -o chalk.md -#
+CALL curl https://raw.githubusercontent.com/chalk/chalk/master/readme.md -o docs\chalk.md -#
 
 ECHO. && ECHO Cheerio:
-CALL curl https://raw.githubusercontent.com/cheeriojs/cheerio/master/Readme.md -o cheerio.md -#
+CALL curl https://raw.githubusercontent.com/cheeriojs/cheerio/master/Readme.md -o docs\cheerio.md -#
 
 ECHO. && ECHO Dotenv:
-CALL curl https://raw.githubusercontent.com/motdotla/dotenv/master/README.md -o dotenv.md -#
+CALL curl https://raw.githubusercontent.com/motdotla/dotenv/master/README.md -o docs\dotenv.md -#
 
 ECHO. && ECHO Inquirer.js:
-CALL curl https://raw.githubusercontent.com/SBoudrias/Inquirer.js/master/README.md -o inquirer.md -#
+CALL curl https://raw.githubusercontent.com/SBoudrias/Inquirer.js/master/README.md -o docs\inquirer.md -#
 
 ECHO. && ECHO Log-Symbols:
-CALL curl https://raw.githubusercontent.com/sindresorhus/log-symbols/master/readme.md -o log-synbols.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/log-symbols/master/readme.md -o docs\log-synbols.md -#
 
 ECHO. && ECHO Signale:
-CALL curl https://raw.githubusercontent.com/klaussinani/signale/master/readme.md -o signale.md -#
+CALL curl https://raw.githubusercontent.com/klaussinani/signale/master/readme.md -o docs\signale.md -#
 
 ECHO. && ECHO Write-Json-File:
-CALL curl https://raw.githubusercontent.com/sindresorhus/write-json-file/master/readme.md -o write-json-file.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/write-json-file/master/readme.md -o docs\write-json-file.md -#
 
 ECHO. && ECHO Done!
 
