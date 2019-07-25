979d0f9d7827b8530d8c1d688b4f5cd2415c01fd
PatEvs <ppevans11@gmail.com>
2019-07-25 21:51:56 +1200

    ✨  Updates to getDocs.cmd

--- scripts/getDocs.cmd
+++ scripts/getDocs.cmd
@@ -2,46 +2,48 @@
 REM Script to download project dependencies documentation
 
 CLS
-ECHO. && ECHO Script to download documentation for project dependencies...
+ECHO. && ECHO Script to download documentation for project dependencies.
 
 :: CALL curl https://raw.githubusercontent.com/user/repository/branch/filename
 
-ECHO. && ECHO Deleting old docs directory...
-RMDIR /S /Q docs
-ECHO Done!
+IF EXIST ..\docs (
+    ECHO. && ECHO Deleting old docs directory...
+    RMDIR /S /Q ..\docs
+    ECHO Done!
+)
 
-ECHO. && ECHO Creating new docs directory...
-CALL mkdir docs
+ECHO. && ECHO Creating docs directory...
+CALL mkdir ..\docs
 ECHO Done!
 
 ECHO. && ECHO Downloading Documentation for Project Dependencies...
 
 ECHO. && ECHO Axios:
-CALL curl https://raw.githubusercontent.com/axios/axios/master/README.md -o docs\axios.md -#
+CALL curl https://raw.githubusercontent.com/axios/axios/master/README.md -o ..\docs\axios.md -#
 
 ECHO. && ECHO Boxen:
-CALL curl https://raw.githubusercontent.com/sindresorhus/boxen/master/readme.md -o docs\boxen.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/boxen/master/readme.md -o ..\docs\boxen.md -#
 
 ECHO. && ECHO Chalk:
-CALL curl https://raw.githubusercontent.com/chalk/chalk/master/readme.md -o docs\chalk.md -#
+CALL curl https://raw.githubusercontent.com/chalk/chalk/master/readme.md -o ..\docs\chalk.md -#
 
 ECHO. && ECHO Cheerio:
-CALL curl https://raw.githubusercontent.com/cheeriojs/cheerio/master/Readme.md -o docs\cheerio.md -#
+CALL curl https://raw.githubusercontent.com/cheeriojs/cheerio/master/Readme.md -o ..\docs\cheerio.md -#
 
 ECHO. && ECHO Dotenv:
-CALL curl https://raw.githubusercontent.com/motdotla/dotenv/master/README.md -o docs\dotenv.md -#
+CALL curl https://raw.githubusercontent.com/motdotla/dotenv/master/README.md -o ..\docs\dotenv.md -#
 
 ECHO. && ECHO Inquirer.js:
-CALL curl https://raw.githubusercontent.com/SBoudrias/Inquirer.js/master/README.md -o docs\inquirer.md -#
+CALL curl https://raw.githubusercontent.com/SBoudrias/Inquirer.js/master/README.md -o ..\docs\inquirer.md -#
 
 ECHO. && ECHO Log-Symbols:
-CALL curl https://raw.githubusercontent.com/sindresorhus/log-symbols/master/readme.md -o docs\log-synbols.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/log-symbols/master/readme.md -o ..\docs\log-synbols.md -#
 
 ECHO. && ECHO Signale:
-CALL curl https://raw.githubusercontent.com/klaussinani/signale/master/readme.md -o docs\signale.md -#
+CALL curl https://raw.githubusercontent.com/klaussinani/signale/master/readme.md -o ..\docs\signale.md -#
 
 ECHO. && ECHO Write-Json-File:
-CALL curl https://raw.githubusercontent.com/sindresorhus/write-json-file/master/readme.md -o docs\write-json-file.md -#
+CALL curl https://raw.githubusercontent.com/sindresorhus/write-json-file/master/readme.md -o ..\docs\write-json-file.md -#
 
 ECHO. && ECHO Done!
 
