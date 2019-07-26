78039afb909ca2afbd7f146c75561b3d03b97227
PatEvs <ppevans11@gmail.com>
2019-07-26 12:48:44 +1200

    ✨  Updates

--- src/index.js
+++ src/index.js
@@ -5,10 +5,29 @@
  *  * Supomation
  */
 
+
 'use strict';
 
+
+
+//----------------------------------------------------------
+// IMPORTS
+//----------------------------------------------------------
+
+const axios = require('axios');
+const chalk = require('chalk');
+const h2t = require('html-to-text');
+const hangingIndent = require('hanging-indent');
+const meow = require('meow');
+const uri = require('uri-js');
+const Multispinner = require('multispinner');
+
+//----------------------------------------------------------
+// Main Logic
+//----------------------------------------------------------
+
 (function () {
-    console.log(" - SUPOMATION CLI - \n");
+    console.log(' - SUPOMATION CLI - \n');
 })();
 
 // EOF //
