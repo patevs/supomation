0aae253ef51722d0d5b05fdc245019755544f977
PatEvs <ppevans11@gmail.com>
2019-07-28 15:53:23 +1200

    ✨  Created src/utils/db.js

---
+++ src/utils/db.js
@@ -0,0 +1,66 @@
+#!/usr/bin/env node
+
+/**
+ *	`src/utils/db.js`
+ *
+ *	* Supomation CLI Database Helper Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// import { MongoClient } from 'mongodb';
+// import assert from 'assert';
+// const assert = require('assert');
+
+// require('dotenv').config();
+// import { config } from 'dotenv';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+// Connection URL
+// const URL = process.env.DB_URL;
+// const url =
+//    'mongodb+srv://patevs:<password>@cluster0-t7g5a.mongodb.net/test?retryWrites=true&w=majority';
+// Database Name
+// const dbName = 'myproject';
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/*
+const connectToDb = () => {
+    const URL = process.env.DB_URL;
+    console.log(URL);
+    const client = new MongoClient(URL, { useNewUrlParser: true });
+    // Use connect method to connect to the server
+    client.connect(function(err) {
+        assert.equal(null, err);
+        console.log('Connected successfully to server');
+
+        // const db =
+        client.db(dbName);
+
+        // Close connect
+        client.close();
+
+        process.exit(0);
+    });
+};
+*/
+
+/*************
+ * * EXPORTS *
+ *************/
+
+// export { connectToDb };
+
+// EOF //
