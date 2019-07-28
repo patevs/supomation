2e144de105879896b99084eb85dbd3531924ee9b
PatEvs <ppevans11@gmail.com>
2019-07-28 15:25:25 +1200

    🚨  Created src/__tests__/

---
+++ src/__tests__/supomation.test.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+/**
+ *  `src/__tests__/supomation.test.js`
+ *
+ *  * Supomation Tests
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// import assert from 'assert';
+// import supomation from "../index.ts";
+
+/****************
+ * * TEST CASES *
+ ****************/
+
+/*
+describe('supomation', () => {
+    it('has a test', () => {
+        assert(true, 'supomation should have a test');
+    });
+});
+*/
+
+// EOF //
