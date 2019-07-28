8265c64fb9bf59c1ebca2782685d0964acb55b34
PatEvs <ppevans11@gmail.com>
2019-07-28 16:15:50 +1200

    ✨  Updates to index.js

--- src/index.js
+++ src/index.js
@@ -21,25 +21,6 @@ const { Select } = require('enquirer');
 const Multispinner = require('multispinner');
 
 /***************
- * * CONSTANTS *
- ***************/
-
-// TODO: Create constants module
-// const BASE_URL = 'https://www.ishopnewworld.co.nz';
-// const CATEGORY_BASE_URL = BASE_URL + '/category/';
-
-// Array of all categories
-/*
-const ALL_CATEGORIES = [
-    'fresh-foods-and-bakery',
-    'chilled-frozen-and-desserts',
-    'pantry',
-    'personal-care',
-    'kitchen-dining-and-household'
-];
-*/
-
-/***************
  * * FUNCTIONS *
  ***************/
 
