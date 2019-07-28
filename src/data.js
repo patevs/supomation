7a731de3d274c923bb8f132020ba1e77b56fe87a
PatEvs <ppevans11@gmail.com>
2019-07-28 16:53:18 +1200

    ✨  Created src/data.js

---
+++ src/data.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+/**
+ *  `src/data.js`
+ *
+ *  * Supomation Data Processing
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+//
+
+/***************
+* * FUNCTIONS *
+***************/
+
+//
+
+/************
+* * EXPORTS *
+*************/
+
+// EOF //
