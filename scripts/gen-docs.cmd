3b6731899ea48e23d0314145d2ff210b70998f29
PatEvs <ppevans11@gmail.com>
2019-08-02 09:46:17 +1200

    ✨  Updates to scripts/gen-docs.cmd

--- scripts/gen-docs.cmd
+++ scripts/gen-docs.cmd
@@ -2,47 +2,52 @@
 REM Script to download project dependencies documentation
 
 CLS
-ECHO. && ECHO [4mRunning script to download dependency documentation[0m
+ECHO. && ECHO [4mRunning Documentation Download Script[0m
 
 :: To download a file from GitHub run:
 :: CALL curl https://raw.githubusercontent.com/user/repository/branch/filename
 
 REM :: Remove old docs directory if exists
 IF EXIST ..\docs (
-    ECHO. && ECHO [96m Deleting old docs directory... [0m
+    ECHO. && ECHO [96m Deleting old [4mdocs[0m [96mdirectory... [0m
     RMDIR /S /Q ..\docs
     ECHO [92m  Done! [0m
 )
 
-REM :: Create docs directory
-ECHO. && ECHO [96m Creating docs directory... [0m
-CALL mkdir ..\docs
+REM :: Create docs\dependencies directory
+ECHO. && ECHO [96m Creating [4mdocs\dependencies[0m [96mdirectory... [0m
+CALL mkdir ..\docs\dependencies
+ECHO [92m  Done! [0m
+
+REM :: Create docs\dev-dependencies directory
+ECHO. && ECHO [96m Creating [4mdocs\dev-dependencies[0m [96mdirectory... [0m
+CALL mkdir ..\docs\dev-dependencies
 ECHO [92m  Done! [0m
 
 ECHO. && ECHO [92mDownloading Documentation for Project Dependencies... [0m
 
-REM :: Move into docs directory
-CALL cd ..\docs
+REM :: Move into docs\dependencies directory
+CALL cd ..\docs\dependencies
 
-ECHO. && ECHO [96maxios: [0m
+ECHO. && ECHO [96m - axios - [0m
 CALL curl https://raw.githubusercontent.com/axios/axios/master/README.md -o axios.md -#
 
-ECHO. && ECHO [96mboxen: [0m
+ECHO. && ECHO [96m - boxen - [0m
 CALL curl https://raw.githubusercontent.com/sindresorhus/boxen/master/readme.md -o boxen.md -#
 
-ECHO. && ECHO [96mchalk: [0m
+ECHO. && ECHO [96m - chalk - [0m
 CALL curl https://raw.githubusercontent.com/chalk/chalk/master/readme.md -o chalk.md -#
 
-ECHO. && ECHO [96mcheerio: [0m
+ECHO. && ECHO [96m - cheerio - [0m
 CALL curl https://raw.githubusercontent.com/cheeriojs/cheerio/master/Readme.md -o cheerio.md -#
 
-ECHO. && ECHO [96mclear: [0m
+ECHO. && ECHO [96m - clear - [0m
 CALL curl https://raw.githubusercontent.com/bahamas10/node-clear/master/README.md -o clear.md -#
 
-ECHO. && ECHO [96mcli-table3: [0m
+ECHO. && ECHO [96m - cli-table3 - [0m
 CALL curl https://raw.githubusercontent.com/cli-table/cli-table3/master/README.md -o cli-table3.md -#
 
-ECHO. && ECHO [96menquirer: [0m
+ECHO. && ECHO [96m - enquirer - [0m
 CALL curl https://raw.githubusercontent.com/enquirer/enquirer/master/README.md -o enquirer.md -#
 
 REM :: ECHO. && ECHO [96mDotenv: [0m
@@ -51,13 +56,13 @@ REM :: CALL curl https://raw.githubusercontent.com/motdotla/dotenv/master/README
 REM :: ECHO. && ECHO [96mInquirer.js: [0m
 REM :: CALL curl https://raw.githubusercontent.com/SBoudrias/Inquirer.js/master/README.md -o inquirer.md -#
 
-ECHO. && ECHO [96mlistr: [0m
+ECHO. && ECHO [96m - listr - [0m
 CALL curl https://raw.githubusercontent.com/SamVerschueren/listr/master/readme.md -o listr.md -#
 
-ECHO. && ECHO [96mlog-symbols: [0m
+ECHO. && ECHO [96m - log-symbols - [0m
 CALL curl https://raw.githubusercontent.com/sindresorhus/log-symbols/master/readme.md -o log-symbols.md -#
 
-ECHO. && ECHO [96mmultispinner: [0m
+ECHO. && ECHO [96m - multispinner - [0m
 CALL curl https://raw.githubusercontent.com/codekirei/node-multispinner/master/readme.md -o multispinner.md -#
 
 REM :: ECHO. && ECHO [96mSignale: [0m
@@ -65,6 +70,8 @@ REM :: CALL curl https://raw.githubusercontent.com/klaussinani/signale/master/re
 
 REM :: ECHO. && ECHO [96mWrite-Json-File: [0m
 REM :: CALL curl https://raw.githubusercontent.com/sindresorhus/write-json-file/master/readme.md -o write-json-file.md -#
+
+GOTO :EOF
 
 ECHO. && ECHO [92mDownloading Documentation for Project Development Dependencies... [0m
 
