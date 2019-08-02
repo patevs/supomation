ffa42fcb0c4af1dc6d13b52728b83542f675a41b
PatEvs <ppevans11@gmail.com>
2019-08-02 12:30:27 +1200

    ✨  Updates to src/scraper/selectors.js

--- src/scraper/selectors.js
+++ src/scraper/selectors.js
@@ -13,8 +13,8 @@
 // Element Selectors
 const PRODUCT_SELECTOR = '.fs-product-card';
 const PRODUCT_DATA_SELECTOR = PRODUCT_SELECTOR + '__footer-container';
-const PRODUCT_NAME_SELECTOR = '.u-p2';
-const PRODUCT_QUANTITY_SELECTOR = '.u-p3';
+// const PRODUCT_NAME_SELECTOR = '.u-p2';
+// const PRODUCT_QUANTITY_SELECTOR = '.u-p3';
 
 /*************
  * * EXPORTS *
@@ -23,8 +23,8 @@ const PRODUCT_QUANTITY_SELECTOR = '.u-p3';
 module.exports = {
     PRODUCT_SELECTOR,
     PRODUCT_DATA_SELECTOR,
-    PRODUCT_NAME_SELECTOR,
-    PRODUCT_QUANTITY_SELECTOR
+    PRODUCT_NAME_SELECTOR: '.u-p2',
+    PRODUCT_QUANTITY_SELECTOR: '.u-p3'
 };
 
 // EOF //
