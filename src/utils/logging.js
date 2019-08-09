320ce1738340aeb7bc64ba5e074a0b527b42fe51
PatEvs <ppevans11@gmail.com>
2019-08-09 17:50:29 +1200

    ✨  Created src/utils/

---
+++ src/utils/logging.js
@@ -0,0 +1,66 @@
+/**
+ *	`src/utils/logging.js`
+ *
+ *	* Supomation CLI Logging Utilities and Helper Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// const chalk = require('chalk');
+// const logSymbols = require('log-symbols');
+// const boxen = require('boxen');
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+// Shortcut to console log
+// const { log } = console;
+
+/***********
+ * * THEME *
+ ***********/
+
+// Colors
+// . const red = chalk.red;
+/*
+const green = chalk.green;
+const blue = chalk.cyan;
+const magenta = chalk.magenta;
+const magentaBright = chalk.magentaBright;
+
+// Typography
+const header = green.bold.underline;
+const title = blue.bold.underline;
+// . const link = blue.underline;
+*/
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+//
+
+/*************
+* * EXPORTS *
+*************/
+
+/*
+module.exports = {
+    log,
+    green,
+    blue,
+    magenta,
+    logError,
+    logInfo,
+    logSuccess,
+    logTitle,
+    logWelcome
+};
+*/
+
+// EOF //
---
+++ src/utils/utilities.js
@@ -0,0 +1,35 @@
+/**
+ *	`src/utils/utilities.js`
+ *
+ *	* Supomation Utility Helper Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+// const logging = require('./logging');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+//
+
+/*************
+* * EXPORTS *
+*************/
+
+/*
+module.exports = {
+    getDate,
+    getDateFull,
+    quit,
+    version,
+    help
+};
+*/
+
+// EOF //
