efa07f1bcfc624489068c39b02901a187403e78c
PatEvs <ppevans11@gmail.com>
2019-08-09 16:53:03 +1200

    📦  Refactoring

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ *  `src/index.js`
+ *
+ *  * Supomation
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+//
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+//
+
+/*****************************
+ * * APPLICATION ENTRY POINT *
+ *****************************/
+
+(() => {
+    console.log('\nWELCOME TO SUPOMATION CLI\n');
+})();
+
+// EOF //
