76803e55e9105a0132a85e306d790ba92e610f94
PatEvs <ppevans11@gmail.com>
2019-08-19 17:14:10 +1200

    ✨  Updates

--- src/.cache/plugins/strapi-plugin-content-manager/admin/src/lifecycles.js
+++ src/.cache/plugins/strapi-plugin-content-manager/admin/src/lifecycles.js
@@ -11,11 +11,11 @@
 import didGetSecuredData from './lifecycles/didGetSecuredData';
 
 function lifecycles() {
-  // Set hooks for the AdminPage container.
-  // Note: we don't need to specify the first argument because we already know what "willSecure" refers to.
-  this.setHooks({
-    didGetSecuredData,
-  });
+    // Set hooks for the AdminPage container.
+    // Note: we don't need to specify the first argument because we already know what "willSecure" refers to.
+    this.setHooks({
+        didGetSecuredData,
+    });
 }
 
 export default lifecycles;
