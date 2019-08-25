0cbbcc22d4bb32eaede07277bb2aeb52f27a621a
PatEvs <ppevans11@gmail.com>
2019-08-25 18:13:28 +1200

    ✨  Updates

--- ui/src/layouts/Home.js
+++ ui/src/layouts/Home.js
@@ -11,12 +11,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-// creates a beautiful scrollbar
+// Creates a beautiful scrollbar
 import PerfectScrollbar from 'perfect-scrollbar';
 // import 'perfect-scrollbar/css/perfect-scrollbar.css';
 // @material-ui/core components
 import withStyles from '@material-ui/core/styles/withStyles';
-// core components
+
+// Components
 // import Navbar from 'components/Navbars/Navbar.jsx';
 // import Footer from 'components/Footer/Footer.jsx';
 // import Sidebar from 'components/Sidebar/Sidebar.jsx';
