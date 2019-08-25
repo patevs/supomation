90615b8d45c65084ce02237f6367ac521aec9533
PatEvs <ppevans11@gmail.com>
2019-08-25 20:55:38 +1200

    ✨  🔥

--- ui/src/index.js
+++ ui/src/index.js
@@ -6,7 +6,8 @@
 /*************
  * * IMPORTS *
  *************/
-import React from 'react';
+
+//  import React from 'react';
 import ReactDOM from 'react-dom';
 import { createBrowserHistory } from 'history';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
