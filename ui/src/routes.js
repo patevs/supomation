dfb8ab5182944d3a446f41ae609d050d5b620dc3
PatEvs <ppevans11@gmail.com>
2019-08-25 21:27:32 +1200

    ✨  Formatting

--- ui/src/routes.js
+++ ui/src/routes.js
@@ -8,6 +8,7 @@
  *************/
 
 // @material-ui/icons
+/*
 import Dashboard from '@material-ui/icons/Dashboard';
 import Person from '@material-ui/icons/Person';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
@@ -17,7 +18,6 @@ import Notifications from '@material-ui/icons/Notifications';
 import Unarchive from '@material-ui/icons/Unarchive';
 import Language from '@material-ui/icons/Language';
 // core components/views for Admin layout
-/*
 import DashboardPage from 'views/Dashboard/Dashboard.jsx';
 import UserProfile from 'views/UserProfile/UserProfile.jsx';
 import TableList from 'views/TableList/TableList.jsx';
