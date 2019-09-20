088b67a607427ef49ee4665a08fc107517b45ad0
PatEvs <ppevans11@gmail.com>
2019-09-20 16:50:10 +1200

    ✨  Updates

--- core/src/index.js
+++ core/src/index.js
@@ -10,13 +10,41 @@
  * * IMPORTS *
  *************/
 
-//
+const globals = require('./globals');
 
 /***************
  * * FUNCTIONS *
  ***************/
 
-//
+/*
+const scrapSpecials = async () => {
+    const targetUrl = globals.SPECIALS_BASE_URL;
+    const specialTasks = new Listr([
+        {
+            title: 'Scraping Category: ' + logging.green('Specials'),
+            task: () => {
+                return new Observable(observer => {
+                    observer.next('Scraping Product Data');
+                    axios.get(targetUrl).then(response => {
+                        const productData = scraper.scrapProductsFromPage(
+                            response.data
+                        );
+                        observer.next('Saving Product Data');
+                        data.saveProductData(
+                            utils.getDate(),
+                            'specials',
+                            productData
+                        );
+                        observer.complete();
+                        // return productData;
+                    });
+                });
+            }
+        }
+    ]);
+    await specialTasks.run();
+};
+*/
 
 /*****************************
  * * APPLICATION ENTRY POINT *
@@ -24,6 +52,7 @@
 
 (() => {
   console.log('Starting Supomation...');
+  console.log('Version: ' + globals.PROJECT_VERSION);
 })();
 
 // EOF //
