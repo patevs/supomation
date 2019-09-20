4a1a1da66bc91408333963d50fe2b52b7f4d1449
PatEvs <ppevans11@gmail.com>
2019-09-20 16:38:45 +1200

    ✨  Updates

---
+++ core/src/globals.js
@@ -0,0 +1,44 @@
+/**
+ *  `src/globals.js`
+ *
+ *  * Supomation Core Global Constants
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const pjson = require('../package.json');
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+// Base URL target
+const BASE_URL = 'https://www.ishopnewworld.co.nz';
+const SPECIALS_BASE_URL = BASE_URL + '/specials';
+const CATEGORY_BASE_URL = BASE_URL + '/category/';
+
+// Array of all categories
+const ALL_CATEGORIES = [
+  'fresh-foods-and-bakery',
+  'chilled-frozen-and-desserts',
+  'pantry',
+  'personal-care',
+  'kitchen-dining-and-household'
+];
+
+/*************
+ * * EXPORTS *
+ *************/
+
+module.exports = {
+  PROJECT_VERSION: pjson.version,
+  SPECIALS_BASE_URL,
+  CATEGORY_BASE_URL,
+  ALL_CATEGORIES
+};
+
+// EOF //
