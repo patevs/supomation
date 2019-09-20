023f6bdca26224860b5084dd10795e6f35f759cc
PatEvs <ppevans11@gmail.com>
2019-09-20 15:36:12 +1200

    ✨  core/src/

---
+++ core/src/__test__/supomation.test.js
@@ -0,0 +1,40 @@
+/**
+ *  `src/__tests__/supomation.test.js`
+ *
+ *  * Supomation Tests
+ *  https://devhints.io/jest
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const assert = require('assert');
+
+/****************
+ * * TEST CASES *
+ ****************/
+
+/*
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+});
+
+describe('My work', () => {
+    test('works', () => {
+        expect(2).toEqual(2);
+    });
+});
+*/
+
+// BDD style test case
+describe('Supomation', () => {
+  // `it` is an alias for `test`.
+  it('Has a test', () => {
+    assert(true, 'Supomation should have a test!');
+  });
+});
+
+// EOF //
