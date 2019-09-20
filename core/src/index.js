527299b087d9fadd6bcb8bbddf7fed556911725c
PatEvs <ppevans11@gmail.com>
2019-09-20 16:43:18 +1200

    ✨  Updates

--- core/src/index.js
+++ core/src/index.js
@@ -13,10 +13,48 @@
 //
 
 /***************
-* * FUNCTIONS *
-***************/
+ * * FUNCTIONS *
+ ***************/
 
 //
+
+/*
+function createAxiosInstance() {
+    // TODO: Allow selection of store id
+    const instance = axios.create({
+        baseURL: VIRTUAL_MAILER_URL,
+        timeout: 1000,
+        headers: { Cookie: "new-world-store-id=storenodeid=1260" }
+    });
+    instance.get(VIRTUAL_MAILER_URL).then(function(response) {
+        const data = response.data;
+        const $ = cheerio.load(data);
+        let catelogScript = $("#__red-pepper-catalog")
+            .next()
+            .html();
+        // Ensure catelogScript is not null
+        if (catelogScript !== null) {
+            catelogScript = catelogScript.trim();
+            const len = catelogScript.length;
+            let catelogId = catelogScript.substr(len - 4, 2);
+            // . logging.log(catelogId);
+        }
+    });
+}
+*/
+
+// --------------------------------------------------------------- //
+
+async function getMailer() {
+  try {
+    const response = await axios.get(
+      "https://app.redpepperdigital.net/app/redpepper/home/91"
+    );
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 /*****************************
  * * APPLICATION ENTRY POINT *
@@ -24,6 +62,7 @@
 
 (() => {
   console.log('Starting Supomation...');
+  getMailer();
 })();
 
 // EOF //
