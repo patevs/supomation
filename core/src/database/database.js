c693bbaed4fe5a7237802651300ccdcca952420d
PatEvs <ppevans11@gmail.com>
2019-09-27 13:03:53 +1200

    ✨  api.js

---
+++ core/src/database/database.js
@@ -0,0 +1,87 @@
+/**
+ *  `src/database/database.js`
+ *
+ *  * Supomation Database Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const helpers = require('./helpers');
+
+/***************
+ * * CONSTANTS *
+ ***************/
+
+const DATA_DIR = 'data/';
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/**
+ *  @function setupDataDir
+ *  @description Ensures the data/date directory exists
+ *  @param { string } date - todays date formatted as: yyyy-mm-dd
+ *  @returns { void }
+ */
+const setupDataDir = date => {
+    helpers.ensureDateDirExists(DATA_DIR, date);
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *  @function saveProductData
+ *  @description Save given product data to file
+ *  @param { string } date - todays date formatted as: yyyy-mm-dd
+ *  @param { string } fileName - name of the data file to save
+ *  @param { object } productData - JSON object containing product data to save
+ *  @returns { boolean }
+ */
+const saveProductData = (date, fileName, productData) => {
+    // Construct file path
+    const filePath = DATA_DIR + date + '/' + fileName + '.json';
+    // Convert JSON data to string
+    const data = JSON.stringify(productData, null, 4);
+    // Write data to file
+    return helpers.writeProductDataFile(filePath, data);
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *  @function loadProductData
+ *  @description Load given product data from disk
+ *  @param { string } date
+ *  @param { string } fileName
+ *  @returns { void }
+ */
+// TODO: Allow for the selection of which data to load
+const loadProductData = (date, fileName) => {
+    // Path to data/date directory
+    let dataDirPath = DATA_DIR + date;
+    // Path to products data file
+    const dataFilePath = dataDirPath + '/' + fileName + '.json';
+    // Read product data file
+    const dataFile = helpers.readProductDataFile(dataFilePath);
+    // Parse product data to JSON
+    const productData = JSON.parse(dataFile);
+    // Display product data in table
+    helpers.displayProductDataTable(productData);
+};
+
+/*************
+ * * EXPORTS *
+ *************/
+
+module.exports = {
+    setupDataDir,
+    saveProductData,
+    loadProductData
+};
+
+// EOF //
---
+++ core/src/utils/utilities.js
@@ -0,0 +1,109 @@
+/**
+ *	`src/utils/utilities.js`
+ *
+ *	* Supomation Utility Helper Functions
+ */
+
+'use strict';
+
+/*************
+ * * IMPORTS *
+ *************/
+
+const logging = require('./logging');
+
+/***************
+ * * FUNCTIONS *
+ ***************/
+
+/**
+ *  @function getDate
+ *  @description Get todays date string formatted as yyyy-mm-dd
+ *  @returns { string } date string
+ */
+const getDate = () => {
+    const d = new Date();
+    return d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *  @function getDateFull
+ *  @description Get todays date string formatted as 'day month date year'
+ *  @returns { string } date string
+ */
+const getDateFull = () => {
+    return new Date().toDateString();
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *  @function welcome
+ *  @description Print the Supomation welcome message
+ *  @param { string } projectVersion
+ *  @returns { void }
+ */
+const welcome = projectVersion => {
+    logging.logWelcome(projectVersion, getDateFull());
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *	@function version
+ *  @description Print the Supomation project version
+ *  @param { string } projectVersion
+ *  @returns { void }
+ */
+const version = projectVersion => {
+    logging.log(); // new line
+    const msg = 'Supomation CLI version: ' + logging.magenta(projectVersion);
+    logging.logInfo(msg);
+    logging.log(); // new line
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *	@function help
+ *  @description Print the Supomation help message
+ *  @returns { void }
+ */
+// TODO: Complete help message
+const help = () => {
+    logging.log(); // new line
+    logging.logInfo(' ---- ' + logging.magenta('HELP MESSAGE') + ' ---- ');
+    logging.log(); // new line
+};
+
+// -------------------------------------------------------- //
+
+/**
+ *  @function quit
+ *  @description Quit Supomation
+ *  @returns { void }
+ */
+const quit = () => {
+    logging.log(); // new line
+    logging.logError('Quitting Supomation CLI...');
+    // throw new Error('Quitting Supomation CLI...');
+    const exit = process.exit;
+    exit(0);
+};
+
+/*************
+ * * EXPORTS *
+ *************/
+
+module.exports = {
+    getDate,
+    getDateFull,
+    welcome,
+    version,
+    help,
+    quit
+};
+
+// EOF //
